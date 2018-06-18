2d3ef004917909799ccce946fe5d9a4cf48dbc10
leiuno <372159394@qq.com>
2018-06-18 23:14:26 +0800

    添加锚点

--- js/main.js
+++ js/main.js
@@ -18,6 +18,17 @@ for(let i = 0; i < liTags.length; i++){
   };
 }
 
+let aTags = document.querySelectorAll('.navBar > ul > li > a');
+for(let i = 0; i < aTags.length; i++){
+  aTags[i].onclick = function(e){
+    e.preventDefault();
+    let a = e.currentTarget;
+    let href = a.getAttribute('href');
+    let element = document.querySelector(href);
+    let top = element.offsetTop;
+    window.scrollTo(0, top - 80);
+  }
+}
 
 
 
